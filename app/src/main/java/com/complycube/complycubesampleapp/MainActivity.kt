b6383241ba9ed7a70e64147e8f3ec591c2967f45
Nasro <nasreddine.daifallah@complycube.com>
2022-11-30 15:31:04 +0100

    Added flow builder.

--- app/src/main/java/com/complycube/complycubesampleapp/MainActivity.kt
+++ app/src/main/java/com/complycube/complycubesampleapp/MainActivity.kt
@@ -9,7 +9,12 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.ComponentActivity
 import com.complycube.complycubesampleapp.databinding.ActivityMainBinding
+import com.complycube.sdk.ComplyCubeSdk
+import com.complycube.sdk.common.data.ClientAuth
+import com.complycube.sdk.common.data.IdentityDocumentType
+import com.complycube.sdk.common.data.Stage
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +23,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var builder = ComplyCubeSdk.Builder(this){
 
+        }
+        // Prepare stages
+        val documentStage = Stage.CustomStage.Document(
+            IdentityDocumentType.Passport(),
+            IdentityDocumentType.ResidencePermit()
+        )
+        builder.withStages(Stage.DefaultStage.Welcome(), documentStage)
+        builder.start(
+            ClientAuth(
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiWm1Rd01tTXhNekl3WldWbU9XSTFOamszWVdOaU1XTXdZV0ZsTVdVek1UVm1ORFF3TldJeE5EVXhPV0U1TkRBM1pEWXdZVGcxT1dRd016RmlZakUwTVRkak56WTBabVUxTVRaa05tRXpaVFF3TUdFM09EWTBZalEyWmpJMk4yWm1aR0k0TW1aa04yWXlOVEk0WW1SbE1XTmhOMkppWmpZeE1tSXdZemhtWTJGaVlqQTNPR1JpTXpGaFpEQmlOREF6TVRNNU16WmxNREF3T0RVM1pXUTVaamxtWkRNM056ZGxaRFkyTUdKalpqSTVaR1kzTlRNd09UY3pOVGRrWm1OaVlqaGxaRFUyWWpSaFl6YzVaalk1TW1VM1ltSTRZelZsTlRBMk5qRXpaalZsTjJWaU1qTTNaVEl3WldRelpXUTVNalpsTldGaFpXWmhZamt5T1RJek5RPT0iLCJ1cmxzIjp7ImFwaSI6Imh0dHBzOi8vYXBpLmNvbXBseWN1YmUuY29tIiwic3luYyI6IndzczovL3hkcy5jb21wbHljdWJlLmNvbSIsImNyb3NzRGV2aWNlIjoiaHR0cHM6Ly94ZC5jb21wbHljdWJlLmNvbSJ9LCJvcHRpb25zIjp7ImhpZGVDb21wbHlDdWJlTG9nbyI6ZmFsc2UsImVuYWJsZUN1c3RvbUxvZ28iOnRydWUsImVuYWJsZVRleHRCcmFuZCI6dHJ1ZSwiZW5hYmxlQ3VzdG9tQ2FsbGJhY2tzIjp0cnVlfSwiaWF0IjoxNjY3NDgzODUzLCJleHAiOjE2Njc0ODc0NTN9.8qnAcJ_t3-_6-JKIzj3q56TE8lJuQOReNBTAYfAvkss"
+            ,"6363c8cc808c610008278c30"
+        )
+        )
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
