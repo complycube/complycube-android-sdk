93a178ba7c37560a2688fab48d9b536f67933a91
Aniket-indulkar <aniket.indulkar@complycube.com>
2025-04-02 15:35:10 +0100

    Kotlin and Gradle upgarded to latest version also latest complycube SDK.

--- app/src/main/java/com/complycube/complycubesampleapp/FirstFragment.kt
+++ app/src/main/java/com/complycube/complycubesampleapp/FirstFragment.kt
@@ -11,6 +11,7 @@ import com.complycube.complycubesampleapp.databinding.FragmentFirstBinding
 import com.complycube.sdk.ComplyCubeSdk
 import com.complycube.sdk.common.data.ClientAuth
 import com.complycube.sdk.common.data.Result
+import com.complycube.sdk.presentation.theme.LookAndFeel
 import com.complycube.sdk.presentation.theme.SdkColors
 
 /**
@@ -28,11 +29,10 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         //Initialize flow
-        complycubeFlow = ComplyCubeSdk.Builder(this,::flowResultHandler).apply {
-            withCustomColors(customColors = SdkColors( primaryButtonColor = Color.RED.toLong()))
-        }
+        complycubeFlow = ComplyCubeSdk.Builder(this,::flowResultHandler)
+        complycubeFlow?.withLookAndFeel(LookAndFeel(enableAnimations = true))
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -42,8 +42,8 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             complycubeFlow?.start(
                 ClientAuth(
-                    token = "SDK TOKEN",
-                    clientId = "CLIENT ID"
+                    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiTTJGa056VTNZamMzWTJKa1lqVTBZelUzTVRFd1pEZzVOV1UxTkdNNE0yWXlaVEZsWXpVMk1XWXhNalkwWWpFek1tTmlOalU0TlRnMU5HRXhOVGs0WWpGak9XTm1PR05rTlRWak9XUTBPVEEyWldNeU9UWTRPV1kzTURZelpEWXpZemRqT0RCbE56azFOVFUwWVRoak1Ea3pNMlV5WTJWaFkyUTVNVEUxTldFek1USTRNV1V3WVRFNU9EY3pPREk1TXpkaU4yVmhZemRqWW1Jd05UZGtZalJrTWpkaU5UVmpaRGczWkdZMVlUVmxOV1E0TnpBMFpUQTFaakZtTWprek1tUXpaR0UxWldZek1EUTNOR1psWkRKak1qUXhNR1JpTXprMFpEUTBPR0ZqWkRZME16Sm1OemhrT0RFM1pETTRZbVF3TW1SaFpqWTFNVFZqT1RNeVpBPT0iLCJ1cmxzIjp7ImFwaSI6Imh0dHBzOi8vYXBpLmNvbXBseWN1YmUuY29tIiwic3luYyI6IndzczovL3hkcy5jb21wbHljdWJlLmNvbSIsImNyb3NzRGV2aWNlIjoiaHR0cHM6Ly94ZC5jb21wbHljdWJlLmNvbSJ9LCJvcHRpb25zIjp7ImhpZGVDb21wbHlDdWJlTG9nbyI6ZmFsc2UsImVuYWJsZUN1c3RvbUxvZ28iOnRydWUsImVuYWJsZVRleHRCcmFuZCI6dHJ1ZSwiZW5hYmxlQ3VzdG9tQ2FsbGJhY2tzIjp0cnVlLCJlbmFibGVOZmMiOmZhbHNlLCJpZGVudGl0eUNoZWNrTGl2ZW5lc3NBdHRlbXB0cyI6NSwiZG9jdW1lbnRJbmZsaWdodFRlc3RBdHRlbXB0cyI6MiwibmZjUmVhZEF0dGVtcHRzIjo1LCJlbmFibGVBZGRyZXNzQXV0b2NvbXBsZXRlIjp0cnVlLCJlbmFibGVXaGl0ZUxhYmVsaW5nIjp0cnVlfSwiaWF0IjoxNzI3MTc0NjU3LCJleHAiOjE3MjcxNzgyNTd9.nNVmg4HL4v6hAoa1GDvOe0Exdaa1dZykqfa63olzi9E",
+                    clientId = "66f27dd9f63537000824e92c"
                 )
             )
         }
